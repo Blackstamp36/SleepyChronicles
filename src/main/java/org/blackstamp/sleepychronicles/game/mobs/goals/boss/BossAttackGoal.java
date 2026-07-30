32be32b503694627a59460bcfbfa85eca43548f5
Blackstamp36 <102997616+Blackstamp36@users.noreply.github.com>
2026-07-30 19:57:48 -0400

    fix: minor fixes to the 'winding_up' code

--- src/main/java/org/blackstamp/sleepychronicles/game/mobs/goals/boss/BossAttackGoal.java
+++ src/main/java/org/blackstamp/sleepychronicles/game/mobs/goals/boss/BossAttackGoal.java
@@ -88,9 +88,9 @@ public class BossAttackGoal extends Goal {
 
         switch(state){
             case BossState.WINDING_UP -> {
-                if(boss.getStateTicks() >= attack.getWindupTicks()){ boss.setState(BossState.ATTACKING); }
-
                 attack.onWindupTick(boss, boss.getStateTicks());
+
+                if(boss.getStateTicks() >= attack.getWindupTicks()){ boss.setState(BossState.ATTACKING); }
             }
 
             case BossState.ATTACKING -> {
