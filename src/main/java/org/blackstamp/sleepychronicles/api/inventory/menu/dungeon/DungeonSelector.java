3082e21c4f5429aac5e901b30c7b8bd47d7c5a19
Blackstamp36 <102997616+Blackstamp36@users.noreply.github.com>
2026-07-31 02:41:08 -0400

    fix: added dungeon selector

---
+++ src/main/java/org/blackstamp/sleepychronicles/api/inventory/menu/dungeon/DungeonSelector.java
@@ -0,0 +1,22 @@
+package org.blackstamp.sleepychronicles.api.inventory.menu.dungeon;
+
+import org.blackstamp.sleepychronicles.api.inventory.MenuTemplate;
+import org.bukkit.entity.Player;
+import org.bukkit.event.inventory.InventoryClickEvent;
+
+public class DungeonSelector extends MenuTemplate {
+
+    public DungeonSelector(Player p, String owner){
+        super(p,owner,"",1);
+    }
+
+    @Override
+    public void initInventory() {
+
+    }
+
+    @Override
+    public void click(InventoryClickEvent e) {
+
+    }
+}
